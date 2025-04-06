f34c3438807e7179ca1f93e248f2b7dc9f792e33
Michael Doyle <michael.james.doyle@gmail.com>
2025-04-05 21:56:13 -0400

    Add dotenv and some additional models

--- apps/model-garden/src/genkit.ts
+++ apps/model-garden/src/genkit.ts
@@ -1,7 +1,25 @@
 import { googleAI } from '@genkit-ai/googleai';
+import vertexAI from '@genkit-ai/vertexai';
+import {
+  claude35Sonnet,
+  claude35SonnetV2,
+  claude3Haiku,
+  claude3Opus,
+  claude3Sonnet,
+  codestral,
+  llama31,
+  llama32,
+  mistralLarge,
+  mistralNemo,
+  vertexAIModelGarden,
+} from '@genkit-ai/vertexai/modelgarden';
 import { genkit } from 'genkit';
 import { logger } from 'genkit/logging';
+import anthropic from 'genkitx-anthropic';
+import cohere from 'genkitx-cohere';
+import groq from 'genkitx-groq';
 import { ollama } from 'genkitx-ollama';
+import openAI from 'genkitx-openai';
 
 logger.setLogLevel('info');
 
@@ -11,42 +29,36 @@ export const ai = genkit({
     ollama({
       models: [
         {
-          name: 'llama3.2:latest',
+          name: 'codestral',
         },
         {
           name: 'gemma3:4b',
-        },
-        {
-          name: 'gemma3:12b',
-        },
-        {
-          name: 'gemma3:27b',
         },
       ],
       serverAddress: 'http://127.0.0.1:11434', // default local address
     }),
 
-    // anthropic(),
-    // cohere(),
-    // groq(),
-    // openAI(),
-    // vertexAI({
-    //   location: 'us-central1',
-    // }),
-    // vertexAIModelGarden({
-    //   location: 'us-central1',
-    //   models: [
-    //     claude35Sonnet,
-    //     claude35SonnetV2,
-    //     claude3Haiku,
-    //     claude3Opus,
-    //     claude3Sonnet,
-    //     codestral,
-    //     llama31,
-    //     llama32,
-    //     mistralLarge,
-    //     mistralNemo,
-    //   ],
-    // }),
+    anthropic(),
+    cohere(),
+    groq(),
+    openAI(),
+    vertexAI({
+      location: 'us-central1',
+    }),
+    vertexAIModelGarden({
+      location: 'us-central1',
+      models: [
+        claude35Sonnet,
+        claude35SonnetV2,
+        claude3Haiku,
+        claude3Opus,
+        claude3Sonnet,
+        codestral,
+        llama31,
+        llama32,
+        mistralLarge,
+        mistralNemo,
+      ],
+    }),
   ],
 });
--- apps/model-garden/src/index.ts
+++ apps/model-garden/src/index.ts
@@ -1,3 +1,7 @@
+import { config } from 'dotenv';
+
+config();
+
 export * from './flows';
 export * from './genkit';
 export * from './tools';
